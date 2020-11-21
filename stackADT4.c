bf9549ae32fd99092a65db49e5866b0629918095
Griffin Heyrich <gheyrich@csa1.bu.edu>
2020-11-20 19:38:13 -0500

    setting up c file for implementation

--- stackADT4.c
+++ stackADT4.c
@@ -7,3 +7,78 @@
 #endif
 #include "memtrace.h"
 
+
+
+typedef struct stack_type *Stack{
+
+
+
+
+}
+
+
+Stack Stack_create(void){
+
+    Stack s = malloc(sizeof(struct style_type));
+    if (s == NULL){
+        printf("Error in create: stack could not be created.");
+        exit(EXIT_FAILURE); 
+    }
+    s->top = NULL;
+    return s;
+}
+
+
+void Stack_destroy(Stack s){
+    Stack_make_empty(s);
+
+
+
+
+}
+
+void Stack_make_empty(Stack s){
+
+
+
+}
+
+
+
+bool Stack_is_empty(Stack s){
+
+
+
+
+}
+
+
+
+bool Stack_is_full(Stack s){
+
+
+
+
+}
+
+
+
+void Stack_push(Stack s, void * i){
+
+
+
+
+}
+
+
+
+void * Stack_pop(Stack s){
+
+
+
+
+
+}
+
+
+
