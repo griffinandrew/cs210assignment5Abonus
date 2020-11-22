b0f75a0069aa5654a1e7dccf3bca3267719aec2c
Griffin Heyrich <gheyrich@csa1.bu.edu>
2020-11-21 19:13:35 -0500

    add book code and adjusted for void type also coms

--- stackADT4.c
+++ stackADT4.c
@@ -9,12 +9,16 @@
 
 
 
+
+
+struct node{
+    void* data;
+    struct node *next
+};
+
 typedef struct stack_type *Stack{
-
-
-
-
-}
+    struct node top*
+};
 
 
 Stack Stack_create(void){
@@ -30,16 +34,15 @@ Stack Stack_create(void){
 
 
 void Stack_destroy(Stack s){
-    Stack_make_empty(s);
-
-
-
-
+    Stack_make_empty(s); //take everything out of stack
+    free(s); //free the space for it 
 }
 
 void Stack_make_empty(Stack s){
 
-
+    while(!is_empty(s)){ //while still items in the stack use pop to remove them all
+        pop(s);
+    }
 
 }
 
@@ -47,38 +50,42 @@ void Stack_make_empty(Stack s){
 
 bool Stack_is_empty(Stack s){
 
-
-
+    return s->top == NULL; //is the top of the stack is full there are no entries in the stack
 
 }
 
 
 
 bool Stack_is_full(Stack s){
-
-
-
+    return false; //stack is never full bc we are using a linked list 
 
 }
-
-
 
 void Stack_push(Stack s, void * i){
 
+    struct node *new_node = malloc(sizeof(struct node)); //allocate space for the node to be pushed
+    if(new_node == NULL){
+        printf("error new node is NULL"\n); //if malloc returns null error occured
+        exit(EXIT_FAILURE); //return failure
+    }
 
-
+    new_node->data = i; //set the new nodes data to given data
+    new_node->next = s->top; //chain newnode to front of the list
+    s->top = new_node; //assign top to newly added node
 
 }
-
 
 
 void * Stack_pop(Stack s){
-
-
-
-
+    struct node *old_top;
+    if(is_empty(s)){
+        printf("error in pop, stack is empty\n"); //stack empty cannot pop
+        exit(EXIT_FAILURE); //return failure
+    }
+    old_top = s->top; //set one to be poped to old top
+    i = old_top->data; //copy old_tops data to be returned
+    s->top = old_top->next; //set top to next to remove old-top from linked list
+    free(old_top); //free the allocated space for it
+    return i; //return tops data
 
 }
-
-
-
